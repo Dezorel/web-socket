6c7b223df9414fa28ef4e3c0a4c9ac9387f7010c
Leonid <scorpion23997@gmail.com>
2021-03-22 20:54:02 +0200

    create logs file

--- test-web-socket/index.php
+++ test-web-socket/index.php
@@ -1,2 +1,13 @@
 <?php
-echo "hello";
+
+require __DIR__.'/vendor/autoload.php'; //прописываем путь к файлу автозагрузки
+
+$log = new Monolog\Logger("name");  // обращаемся к классам (в папке монолог)
+$log->pushHandler(new Monolog\Handler\StreamHandler( //создаём обработчик, как именно будет сохраняться записи (логи);
+    'app.log',       //создаём файл app.log
+    Monolog\Logger::INFO         //записываем логи уровня Warning
+//Так же существуют другие типы: Debug, info, notice, warning, error, critical, alert, emergency
+));
+$log->addInfo("Test");   //помещаем запись (слово Тест)
+
+
