038cdc53ce20453fa75ab73dc970a97eab4535f8
Leonid <scorpion23997@gmail.com>
2021-03-31 21:53:07 +0300

    Update test1.php

--- OOP/test1.php
+++ OOP/test1.php
@@ -16,12 +16,12 @@
 
 //----------------------------
 
-class Animals{
+class Animal{
 
-    private int $age;
+    private float $age;
     public string $name;
 
-    public function __construct(string $name, int $age = 0){
+    public function __construct(string $name, float $age = 0){
         $this->age = $age;
         $this->name = $name;
     }
@@ -31,15 +31,28 @@ class Animals{
     }
 }
 
-class Dog extends Animals{
+class Dog extends Animal{
     public string $breed;
-    public function __construct(string $name, string $breed, int $age = 0)
+    public function __construct(string $name, string $breed, int $age = 0)      //желательно - параметры по умолчанию
+                                                                                    // пихать в самый конец
     {
-        parent::__construct($name,$age,);
+        parent::__construct($name,$age);
         $this->breed = $breed;
     }
-    
+    public function Say()
+    {
+        parent::Say();          //вызывает уже существующую функцию
+        echo " || Name = ".$this->name;
+        echo " || Breed = ".$this->breed;
+        echo "\n";
+    }
 }
 
-$tax = new Dog(breed:'taxa',age: 12,name: 'Alex');
+$tax = new Dog(breed:'taxa',age: 2,name: 'Alex');
 $tax->Say();
+
+$buldog = new Dog(age: 3,name:'Semion', breed: 'buldog');
+$buldog->Say();
+
+$cat = new Animal(age: 0.5, name:'Eddy');
+$cat->Say();
