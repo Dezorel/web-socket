bd1e2fdb34306b6041531db712d49c478d7e4068
Leonid <scorpion23997@gmail.com>
2021-03-31 21:45:16 +0300

    Update test1.php

--- OOP/test1.php
+++ OOP/test1.php
@@ -1,15 +1,45 @@
 <?php
 
-class test1{
-    public $temp = 12;
+//class test1{
+//    public $temp = 12;
+//
+//    public function NewPrint(){
+//        $var = 7;
+//        echo $this->temp." | ";
+//        echo $var;
+//    }
+//}
+//
+//
+//$a = new test1();
+//$a->NewPrint();
 
-    public function NewPrint(){
-        $var = 7;
-        echo $this->temp." | ";
-        echo $var;
+//----------------------------
+
+class Animals{
+
+    private int $age;
+    public string $name;
+
+    public function __construct(string $name, int $age = 0){
+        $this->age = $age;
+        $this->name = $name;
+    }
+
+    public function Say(){
+        echo "Age = ".$this->age;
     }
 }
 
+class Dog extends Animals{
+    public string $breed;
+    public function __construct(string $name, string $breed, int $age = 0)
+    {
+        parent::__construct($name,$age,);
+        $this->breed = $breed;
+    }
+    
+}
 
-$a = new test1();
-$a->NewPrint();
+$tax = new Dog(breed:'taxa',age: 12,name: 'Alex');
+$tax->Say();
