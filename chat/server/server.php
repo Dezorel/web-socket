68cfd8dde43a141c6f753389e49d9691b16793a6
Leonid <scorpion23997@gmail.com>
2021-04-01 20:56:21 +0300

    socket

--- chat/server/server.php
+++ chat/server/server.php
@@ -1,1 +1,23 @@
 <?php
+use Workerman\Worker;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$socket = new Worker('websocket://0.0.0.0:2346');
+$socket->count = 5;
+
+$socket->onConnect = function ($connection){
+    echo "Create new user \n";
+};
+
+$socket->onMessage = function ($connection, $data) use ($socket){
+    foreach($socket->connections as $clientConnection){
+        $clientConnection->send($data);
+    }
+};
+
+$socket->onClose = function ($connection){
+    echo "User deleted\n";
+};
+
+Worker::runAll();
