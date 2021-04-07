e7455ef71ae23fbcf93276380197f896f9232c6c
Leonid <scorpion23997@gmail.com>
2021-04-08 00:35:32 +0300

    create condition

--- TelegramBot/index.php
+++ TelegramBot/index.php
@@ -8,29 +8,31 @@ $response = json_decode(
     JSON_OBJECT_AS_ARRAY
 );
 
-$url1 = "https://api.telegram.org/bot".TOKEN."/getUpdates";
-$lastUpdatedID = getFirstUnviewMessage($response);
-$params = [
-    'offset'=>$lastUpdatedID+1
-];
-$url1 = $url1 . '?' . http_build_query($params);
-$response1 = json_decode(
-    file_get_contents($url1),
-    JSON_OBJECT_AS_ARRAY
-);
+if(count($response['result'])> 0){
+    $url1 = "https://api.telegram.org/bot".TOKEN."/getUpdates";
+    $lastUpdatedID = getFirstUnviewMessage($response);
+    $params = [
+        'offset'=>$lastUpdatedID+1
+    ];
+    $url1 = $url1 . '?' . http_build_query($params);
+    $response1 = json_decode(
+        file_get_contents($url1),
+        JSON_OBJECT_AS_ARRAY
+    );
 
-var_dump($lastUpdatedID);
 
-if($response1['ok']){
-    foreach ($response1['result'] as $update) {
-        echo $update['message']['text'];
-        echo "<br>";
+
+    if($response1['ok']){
+        foreach ($response1['result'] as $update) {
+            echo $update['message']['text'];
+            echo "<br>";
+        }
+    }else{
+        echo 'not found';
     }
-}else{
-    echo 'not found';
+
 }
-
-
+var_dump($lastUpdatedID);
 
 
 
