f53bcf5c1bc467257657cb5efcaf5dff7528c2f5
Leonid <scorpion23997@gmail.com>
2021-04-07 23:36:21 +0300

    Update index.php

--- TelegramBot/index.php
+++ TelegramBot/index.php
@@ -2,8 +2,17 @@
 require "token.php";
 
 $url = "https://api.telegram.org/bot".TOKEN."/getUpdates";
+$lastUpdatedID = 303555033;
 
-$response = json_decode(file_get_contents($url), JSON_OBJECT_AS_ARRAY);
+$params = [
+    'offset'=>$lastUpdatedID        //+1
+];
+$url = $url . '?' . http_build_query($params);
+
+$response = json_decode(
+            file_get_contents($url),
+    JSON_OBJECT_AS_ARRAY
+);
 
 if($response['ok']){
     foreach ($response['result'] as $update) {
