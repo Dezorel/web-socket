432b3d3c8d49861fb0bbfca3d4776ac43170fc2f
Leonid <scorpion23997@gmail.com>
2021-04-08 00:32:03 +0300

    create loop unview message

--- TelegramBot/index.php
+++ TelegramBot/index.php
@@ -2,23 +2,38 @@
 require "token.php";
 
 $url = "https://api.telegram.org/bot".TOKEN."/getUpdates";
-$lastUpdatedID = 303555033;
-
-$params = [
-    'offset'=>$lastUpdatedID        //+1
-];
-$url = $url . '?' . http_build_query($params);
 
 $response = json_decode(
             file_get_contents($url),
     JSON_OBJECT_AS_ARRAY
 );
 
-if($response['ok']){
-    foreach ($response['result'] as $update) {
+$url1 = "https://api.telegram.org/bot".TOKEN."/getUpdates";
+$lastUpdatedID = getFirstUnviewMessage($response);
+$params = [
+    'offset'=>$lastUpdatedID+1
+];
+$url1 = $url1 . '?' . http_build_query($params);
+$response1 = json_decode(
+    file_get_contents($url1),
+    JSON_OBJECT_AS_ARRAY
+);
+
+var_dump($lastUpdatedID);
+
+if($response1['ok']){
+    foreach ($response1['result'] as $update) {
         echo $update['message']['text'];
         echo "<br>";
     }
 }else{
     echo 'not found';
+}
+
+
+
+
+
+function getFirstUnviewMessage($response){
+    return $response['result'][0]['update_id'];
 }
--- TelegramBot/send.php
+++ TelegramBot/send.php
@@ -1,27 +1,19 @@
 <?php
 require "token.php";
+
 $urlSend = "https://api.telegram.org/bot".TOKEN."/sendMessage";
-$phrasesHi = ['Привет', 'Здравствуй!', 'Добро пожаловать Линуксоид', 'Я ждал тебя, мой подаван'];
+$phrasesHi = ['Привет', 'Здравствуй!', 'Добро пожаловать Линуксоид', 'Я ждал тебя, мой подаван', 'У меня припасено много банальных фраз'];
+$phrasesAngry = ['Тебе здесь не рады!', 'Не правда! Линукс лучшая ОС всех времё и народов'];
 
 $params = [
     'chat_id'=>1534045363,
     'text'=>randomPhrase($phrasesHi)
 ];
-
 $urlSend = $urlSend . '?' . http_build_query($params);
 $response = json_decode(
     file_get_contents($urlSend),
     JSON_OBJECT_AS_ARRAY
 );
-
-if($response['ok']){
-    foreach ($response['result'] as $update) {
-        echo $update['message']['text'];
-        echo "<br>";
-    }
-}else{
-    echo 'not found';
-}
 
 function randomPhrase($phrases){
     $num = rand(0, count($phrases)-1);
