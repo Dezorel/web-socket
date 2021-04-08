cb0199328cb0d7ecf2b979ce5219225ec0af2875
Leonid <scorpion23997@gmail.com>
2021-04-08 12:51:50 +0300

    create auto refresh and add send message last user

--- TelegramBot/index.php
+++ TelegramBot/index.php
@@ -1,41 +1,55 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="refresh" content="2">
+    <title>Bot logs</title>
+</head>
+<body>
+
+</body>
+</html>
+
 <?php
-require "token.php";
+require ('token.php');
+require ('send.php');
 
-$url = "https://api.telegram.org/bot".TOKEN."/getUpdates";
-
-$response = json_decode(
-            file_get_contents($url),
-    JSON_OBJECT_AS_ARRAY
-);
-
-if(count($response['result'])> 0){
-    $url1 = "https://api.telegram.org/bot".TOKEN."/getUpdates";
-    $lastUpdatedID = getFirstUnviewMessage($response);
-    $params = [
-        'offset'=>$lastUpdatedID+1
-    ];
-    $url1 = $url1 . '?' . http_build_query($params);
-    $response1 = json_decode(
-        file_get_contents($url1),
+function getUpdates(){
+    $url = "https://api.telegram.org/bot".TOKEN."/getUpdates";
+    $response = json_decode(
+        file_get_contents($url),
         JSON_OBJECT_AS_ARRAY
     );
+    if(count($response['result'])> 0){
+        $url1 = "https://api.telegram.org/bot".TOKEN."/getUpdates";
+        $lastUpdatedID = getFirstUnviewMessage($response);
+        sendMessage($response['result'][0]['message']['chat']['id']);
+        $params = [
+            'offset'=>$lastUpdatedID+1
+        ];
+        $url1 = $url1 . '?' . http_build_query($params);
+        $response1 = json_decode(
+            file_get_contents($url1),
+            JSON_OBJECT_AS_ARRAY
+        );
 
-    var_dump($lastUpdatedID);
-
-    if($response1['ok']){
-        foreach ($response1['result'] as $update) {
-            echo $update['message']['text'];
-            echo "<br>";
+        if($response1['ok']){
+            echo 'Не обработанные сообщения:<br>';
+            foreach ($response1['result'] as $update) {
+                echo $update['message']['text'];
+                echo "<br>";
+            }
+        }else{
+            echo 'not found';
         }
-    }else{
-        echo 'not found';
+
     }
-
+    else{
+        echo '<h1 align="center">No unread messages</h1>';
+    }
 }
-else{
-    echo '<h1 align="center">No unread messages</h1>';
-}
 
+getUpdates();
 
 
 
--- TelegramBot/send.php
+++ TelegramBot/send.php
@@ -1,20 +1,26 @@
 <?php
-require "token.php";
+
 
 $urlSend = "https://api.telegram.org/bot".TOKEN."/sendMessage";
 $phrasesHi = ['Привет', 'Здравствуй!', 'Добро пожаловать Линуксоид', 'Я ждал тебя, мой подаван', 'У меня припасено много банальных фраз'];
 $phrasesAngry = ['Тебе здесь не рады!', 'Не правда! Линукс лучшая ОС всех времё и народов'];
 
-$params = [
-    'chat_id'=>1534045363,
-    'text'=>randomPhrase($phrasesHi)
-];
-$urlSend = $urlSend . '?' . http_build_query($params);
-$response = json_decode(
-    file_get_contents($urlSend),
-    JSON_OBJECT_AS_ARRAY
-);
+function sendMessage($chat_id){
+    global $phrasesHi;
+    global $phrasesAngry;
+    global $urlSend;
 
+    $params = [
+        'chat_id'=>$chat_id,
+        'text'=>randomPhrase($phrasesHi)
+    ];
+    $urlSend = $urlSend . '?' . http_build_query($params);
+    $response = json_decode(
+        file_get_contents($urlSend),
+        JSON_OBJECT_AS_ARRAY
+    );
+
+}
 function randomPhrase($phrases){
     $num = rand(0, count($phrases)-1);
     return $phrases[$num];
