d8316ef5fd233584c0924186f840e3b17c25b319
Leonid <scorpion23997@gmail.com>
2021-04-27 22:10:59 +0300

    add how are you message and refactore some code

--- TelegramBot/send.php
+++ TelegramBot/send.php
@@ -6,10 +6,12 @@ require ('phrases.php');
 
 function sendMessage($chat_id, $userMessage){
 
-    $noHiCur = sayHi($chat_id, $userMessage);
-    $noAnCur = Angry($chat_id, $userMessage);
+    $NotHiPhrase = sayHi($chat_id, $userMessage);
+    $NotAngryPhrase = Angry($chat_id, $userMessage);
+    $NotHAYPhrase = HowAreYou($chat_id, $userMessage);
 
-    if($noHiCur==0 && $noAnCur==0){
+
+    if($NotHiPhrase==0 && $NotAngryPhrase==0 && $NotHAYPhrase == 0){
         noSayHi($chat_id);
     }
 }
@@ -18,69 +20,75 @@ function sayHi($chat_id, $userMessage): int
 {
     global $phrasesHi;
     global $usersPhrasesHi;
-    global $urlSend;
+
     $userMessage = mb_strtolower($userMessage);
-    $noHi = count($usersPhrasesHi);
+    $countOfPhrase = count($usersPhrasesHi);
 
     foreach ($usersPhrasesHi as $usi){
         if(strnatcasecmp($userMessage, $usi)==0){
-            $params = [
-                'chat_id'=>$chat_id,
-                'text'=>randomPhrase($phrasesHi)
-            ];
-            $urlSend = $urlSend . '?' . http_build_query($params);
-            $response = json_decode(
-                file_get_contents($urlSend),
-                JSON_OBJECT_AS_ARRAY
-            );
+            sendRandomPhrase($chat_id, $phrasesHi);
+
             break;
         }
-        $noHi--;
+        $countOfPhrase--;
     }
-    return $noHi;
+    return $countOfPhrase;
 }
 
 function Angry($chat_id, $userMessage): int
 {
     global $phrasesAngry;
     global $usersPhrasesAngry;
-    global $urlSend;
+
     $userMessage = mb_strtolower($userMessage);
-    $noHi = count($usersPhrasesAngry);
+    $countOfPhrase = count($usersPhrasesAngry);
 
     foreach ($usersPhrasesAngry as $usa){
         if(strnatcasecmp($userMessage, $usa)==0){
-            $params = [
-                'chat_id'=>$chat_id,
-                'text'=>randomPhrase($phrasesAngry)
-            ];
-            $urlSend = $urlSend . '?' . http_build_query($params);
-            $response = json_decode(
-                file_get_contents($urlSend),
-                JSON_OBJECT_AS_ARRAY
-            );
+            sendRandomPhrase($chat_id,$phrasesAngry);
             break;
         }
-        $noHi--;
+        $countOfPhrase--;
     }
-    return $noHi;
+    return $countOfPhrase;
 }
+
+function HowAreYou($chat_id, $userMessage): int
+{
+    global $phrasesHowAreYou;
+    global $userPhrasesHowAreYou;
+
+    $userMessage = mb_strtolower($userMessage);
+    $countOfPhrase = count($userPhrasesHowAreYou);
+
+    foreach ($userPhrasesHowAreYou as $ushay){
+        if(strnatcasecmp($userMessage, $ushay)==0){
+            sendRandomPhrase($chat_id, $phrasesHowAreYou);
+            break;
+        }
+        $countOfPhrase--;
+    }
+    return $countOfPhrase;
+}
+
 
 function noSayHi($chat_id){
     global $notHiPhrase;
+    sendRandomPhrase($chat_id, $notHiPhrase);
+}
+function randomPhrase($phrases){
+    $num = rand(0, count($phrases)-1);
+    return $phrases[$num];
+}
+function sendRandomPhrase($chat_id, $phrases){
     global $urlSend;
     $params = [
         'chat_id'=>$chat_id,
-        'text'=>randomPhrase($notHiPhrase)
+        'text'=>randomPhrase($phrases)
     ];
     $urlSend = $urlSend . '?' . http_build_query($params);
     $response = json_decode(
         file_get_contents($urlSend),
         JSON_OBJECT_AS_ARRAY
     );
-}
-
-function randomPhrase($phrases){
-    $num = rand(0, count($phrases)-1);
-    return $phrases[$num];
 }
