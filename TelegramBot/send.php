962d0f4863db4941a8dbc1d4b6015247c3891769
Leonid <scorpion23997@gmail.com>
2021-04-27 21:49:02 +0300

    update logic

--- TelegramBot/send.php
+++ TelegramBot/send.php
@@ -2,28 +2,33 @@
 
 
 $urlSend = "https://api.telegram.org/bot".TOKEN."/sendMessage";
-$phrasesHi = ['Привет', 'Здравствуй!', 'Добро пожаловать Линуксоид', 'Я ждал тебя, мой подаван', 'У меня припасено много банальных фраз для приветсвия'];
-$usersPhrasesHi = ['Привет', 'привет', 'Здравствуй','здравствуй', 'Ку', 'ку','Куку', 'куку', 'Бонжур','бонжур', 'Хай', 'хай', 'Hello','hello', 'Hi','hi'];
+
+$phrasesHi = ['Привет', 'Здравствуй!', 'Добро пожаловать Линуксоид', 'Я ждал тебя, мой подаван', 'У меня припасено 
+много банальных фраз для приветсвия'];
+$usersPhrasesHi = ['привет', 'здравствуй', 'ку', 'куку','бонжур', 'хай', 'hello', 'hi'];
 
 $phrasesAngry = ['Тебе здесь не рады!', 'Не правда! Линукс лучшая ОС всех времё и народов'];
-$usersPhrasesAngry = ['Linux говно', 'Linux govno', 'Linux sheet', 'я пользуюсь windows','Я пользуюсь windows','Я пользуюсь Windows','я пользуюсь Windows',];
-$notHiPhrase = ['А поздороваться?', 'Со мной никто не здоровается...', 'Причина востания машин: Отсутсвие приветсвия пользователя'];
+$usersPhrasesAngry = ['linux говно', 'linux govno', 'linux sheet', 'я пользуюсь windows','линукс говно'
+];
+$notHiPhrase = ['А поздороваться?', 'Со мной никто не здоровается...', 'Причина востания машин: Отсутсвие приветсвия 
+пользователя', 'Не понимаю тебя', 'It\'s hard to understand you'];
 
 function sendMessage($chat_id, $userMessage){
-    global $notHiPhrase;
-    global $urlSend;
+
     $noHiCur = sayHi($chat_id, $userMessage);
     $noAnCur = Angry($chat_id, $userMessage);
+
     if($noHiCur==0 && $noAnCur==0){
-
+        noSayHi($chat_id);
     }
-
 }
 
-function sayHi($chat_id, $userMessage){
+function sayHi($chat_id, $userMessage): int
+{
     global $phrasesHi;
     global $usersPhrasesHi;
     global $urlSend;
+    $userMessage = mb_strtolower($userMessage);
     $noHi = count($usersPhrasesHi);
 
     foreach ($usersPhrasesHi as $usi){
@@ -44,10 +49,12 @@ function sayHi($chat_id, $userMessage){
     return $noHi;
 }
 
-function Angry($chat_id, $userMessage){
+function Angry($chat_id, $userMessage): int
+{
     global $phrasesAngry;
     global $usersPhrasesAngry;
     global $urlSend;
+    $userMessage = mb_strtolower($userMessage);
     $noHi = count($usersPhrasesAngry);
 
     foreach ($usersPhrasesAngry as $usa){
@@ -68,11 +75,9 @@ function Angry($chat_id, $userMessage){
     return $noHi;
 }
 
-function noSayHi($chat_id, $userMessage){
-
+function noSayHi($chat_id){
     global $notHiPhrase;
     global $urlSend;
-
     $params = [
         'chat_id'=>$chat_id,
         'text'=>randomPhrase($notHiPhrase)
